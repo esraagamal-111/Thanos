c6e4f4125b13fc7b541a415e56c245712777183b
esraagamal-111 <esramgamal@gmail.com>
2021-07-21 19:52:19 +0200

    Remove Random Files

--- remove_random_file.py
+++ remove_random_file.py
@@ -2,20 +2,18 @@ import create_backup_dir
 import os
 import random
 
-create_backup_dir.src_dir_loc = "E:/Courses/Data Science/Epsilon/Lecture_4_Thanos_Project/Thanos_Project/Universe"
-create_backup_dir.bkp_dir_loc = "E:/Courses/Data Science/Epsilon/Lecture_4_Thanos_Project/Thanos_Project/Backup/"
+src_dir_loc = "E:/Courses/Data Science/Epsilon/Lecture_4_Thanos_Project/Thanos_Project/Universe"
+bkp_dir_loc = "E:/Courses/Data Science/Epsilon/Lecture_4_Thanos_Project/Thanos_Project/Backup/"
 
-src_dir= create_backup_dir.src_dir_loc
-bkp_dir= create_backup_dir.bkp_dir_loc
 # call perfrom_backup function & create backup
-create_backup_dir.perform_backup(src_dir,bkp_dir)
+create_backup_dir.perform_backup(src_dir_loc,bkp_dir_loc)
 
 # get list of folder files to remove random 
-folder_files = os.listdir(src_dir)
+folder_files = os.listdir(src_dir_loc)
 # check folder files before removing files
 print(len(folder_files))
 
 # looping on directory & remove 50% of random files 
 for file in random.sample(folder_files,int(len(folder_files)/2)):
-    os.remove(os.path.join(src_dir,file))
-print("Files removed successfuly, folder contains{}".format(len(os.listdir(src_dir))))
+    os.remove(os.path.join(src_dir_loc,file))
+print("Files removed successfuly, folder contains{}".format(len(os.listdir(src_dir_loc))))
